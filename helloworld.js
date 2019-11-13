5ec7f4362bf899121e0f9db4fffecb5baafd65db
burton.machado@gmail.com <burton.machado@gmail.com>
2019-11-12 23:03:49 -0500

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+function myfun(num) {
+    console.log("This is : " + num);
+}
+myfun(77);
+var message = 'Hello World';
+console.log(message);
+var num = 7;
+console.log(num);
---
+++ helloworld.ts
@@ -0,0 +1,8 @@
+function myfun(num:number) {
+    console.log("This is : " + num);
+}
+myfun(77);
+let message: string = 'Hello World';
+console.log(message);
+let num:number = 7;
+console.log(num);
